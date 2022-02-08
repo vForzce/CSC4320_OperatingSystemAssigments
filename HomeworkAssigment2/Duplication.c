691d6635bc1e618591ebc21b246e3c3a5ec92588
Victor Wilberforce <victorwilberforce2310@gmail.com>
2022-02-08 15:02:28 -0500

    Dummy code

--- HomeworkAssigment2/Duplication.c
+++ HomeworkAssigment2/Duplication.c
@@ -3,14 +3,42 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<stdlib.h>
+ 
 
+void process() {
+    char *path = "IngterFile.txt"; //Creating a File
+    FILE *fp = fopen(path, "r"); //Open the File 
+    int num;
+    pid_t pid; // Get Process Id
+    fscanf (fp, "%d", &num); //Read Number
+    fclose (fp); //Close file
+    if ((pid = getpid()) < 0) { //Get Process ID
+       printf("unable to get pid");
+    } else {
+       printf("N: %d Process ID: %d\n",num,pid);
+    }
+    fp = fopen(path, "w");
+    num++;
+    // putw(num,fp);
+    fprintf(fp, "%d", num);
+    fflush(fp);
+    fclose(fp);
+}
 
 int main(void) {
-    char *path = "IngterFile";
-    FILE *fp = fopen(path, "r");
-    int num;
-    if(fork() == 0) {
-        printf("Process A has started\n");
-    }
-    
+     if (fork() == 0) {
+       printf("Starting Process A\n");
+       process();
+   }
+   if (fork() == 0) {
+       printf("Starting Process B\n");
+       process();
+   }
+   if (fork() == 0){
+       printf("Starting Process C\n");
+       process();
+   }
+   return 0;
 }
+
+    
