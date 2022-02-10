a74aaa19871c8ad931e1c918f03332d7c86108e6
Victor Wilberforce <victorwilberforce2310@gmail.com>
2022-02-10 11:10:14 -0500

    Finally got it to work

--- HomeworkAssigment2/Duplication.c
+++ HomeworkAssigment2/Duplication.c
@@ -3,28 +3,33 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<stdlib.h>
- 
 
+const char* path = "Intger.txt"; //Creating a File
 
 int main() {
     if(fork() == 0){
-        printf("Starting process B\n");
+        printf("Starting process C\n");
         if(fork() == 0){
-            printf("Staring process A\n");
+            printf("Staring process B\n");
+            if(fork()== 0){
+                printf("Starting process A\n");
+            }
         }
     }
 
     pid_t processid = getpid();
-    char* path = "IngterFile.txt"; //Creating a File
-    for(int i = 0; i < 200; i++) {
+    
+    for(int i = 0; i <=200; i++) {
         FILE* fp = fopen(path, "r"); //Open the File 
-        int num = 0;
+        int num;
         fscanf (fp, "%d", &num); //Read Number
+        rewind(fp);
         fclose (fp); 
         printf("N: %d Process ID: %d\n", num, processid);
-        fp = fopen(path, "a");
         num++;
+        fp = fopen(path, "w");
         fprintf(fp, "%d", num);
+        rewind(fp);
         fclose(fp);
     }  
 }
